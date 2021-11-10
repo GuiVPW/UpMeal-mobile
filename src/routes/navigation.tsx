de0f9caf1381848275bb2019a4900217249e9267
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-11-10 18:14:04 -0300

    feat: :sparkles: add storage check and map screen
    use expo secure store to check if it is a new user or not, and if it is, redirect to home screen instead of map screen

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -1,25 +1,54 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { AppStack } from './stack'
+import React, { useEffect, useState } from 'react'
+
 import { useTheme } from 'styled-components'
-import { HOME } from './routes'
-import HomeScreen from '../screens/Home'
+
+import { NavigationContainer } from '@react-navigation/native'
+import AppLoading from 'expo-app-loading'
+import SecureStore from 'expo-secure-store'
+
+import { HomeScreen } from '../screens/Home'
+import { MapScreen } from '../screens/Map'
+import { HOME, MAP } from './routes'
+import { AppStack } from './stack'
 
 export const Navigation = () => {
 	const theme = useTheme()
+	const [isNew, setIsNew] = useState<boolean>()
+
+	useEffect(() => {
+		;(async () => {
+			try {
+				const item = await SecureStore.getItemAsync('is_new')
+
+				if (!item) {
+					setIsNew(true)
+				} else {
+					setIsNew(JSON.parse(item))
+				}
+			} catch (e) {
+				console.log(e)
+				setIsNew(true)
+			}
+		})()
+	}, [])
+
+	if (typeof isNew === 'undefined') {
+		return <AppLoading />
+	}
 
 	return (
 		<NavigationContainer>
 			<AppStack.Navigator
+				initialRouteName={isNew ? HOME : MAP}
 				screenOptions={{
+					headerShown: false,
 					contentStyle: {
 						backgroundColor: theme.background.main
 					}
 				}}
 			>
 				<AppStack.Screen name={HOME} component={HomeScreen} />
-				{/* <AppStack.Screen name="Home" component={Home} />
-				<AppStack.Screen name="Points" component={Points} />
-				<AppStack.Screen name="Detail" component={Detail} /> */}
+				<AppStack.Screen name={MAP} component={MapScreen} />
 			</AppStack.Navigator>
 		</NavigationContainer>
 	)
