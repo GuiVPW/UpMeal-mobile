a03565ca6a8f1aad5099ef4a0a1eff5ae77a1526
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-11-10 15:43:28 -0300

    feat: add application theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+	background: {
+		main: '#f0f0f5',
+		secondary: '#d7d7d7',
+		light: '#fff'
+	},
+	colors: {
+		primary: '#fe7240'
+	},
+	text: {
+		main: '#6c6c80',
+		secondary: '#A8A8B3'
+	},
+	title: {
+		main: '#322153',
+		secondary: '#41414D'
+	},
+	border: {
+		divider: '1px solid #F0F0F5'
+	},
+	input: {
+		background: '#f0f0f5',
+		placeholder: '#a0a0b2',
+		text: '#6c6c80'
+	}
+}
+
+export type DefaultTheme = typeof theme
