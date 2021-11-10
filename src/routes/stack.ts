c12f5eccaf28dde783b47ce8dbe65ce0371815bd
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-11-10 18:12:30 -0300

    feat: :memo: add react navigation type checking
    add root param list, root stack param list and screen props

--- src/routes/stack.ts
+++ src/routes/stack.ts
@@ -1,3 +1,25 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {
+	createNativeStackNavigator,
+	NativeStackScreenProps
+} from '@react-navigation/native-stack'
+import { ParamListBase } from '@react-navigation/routers'
 
-export const AppStack = createNativeStackNavigator()
+export interface RootStackParamList extends ParamListBase {
+	Home: undefined
+	Map: undefined
+}
+
+export type ScreenProps<T extends keyof RootStackParamList> = NativeStackScreenProps<
+	RootStackParamList,
+	T
+>
+
+declare global {
+	// eslint-disable-next-line @typescript-eslint/no-namespace
+	namespace ReactNavigation {
+		// eslint-disable-next-line @typescript-eslint/no-empty-interface
+		interface RootParamList extends RootStackParamList {}
+	}
+}
+
+export const AppStack = createNativeStackNavigator<RootStackParamList>()
