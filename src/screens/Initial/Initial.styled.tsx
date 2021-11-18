a0385b852ca8635ffb523bbc0e9407f388dfe0b7
Guilherme Vieira <guivpw68@gmail.com>
2021-11-18 05:05:54 -0300

    feat: add login input

--- src/screens/Initial/Initial.styled.tsx
+++ src/screens/Initial/Initial.styled.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@react-navigation/native'
 import { Headline, Subheading, TouchableRipple } from 'react-native-paper'
 import styled from 'styled-components/native'
 
@@ -10,8 +11,8 @@ export const Container = styled.View`
 `
 
 export const MainImage = styled.Image`
-	width: 200px;
-	height: 200px;
+	width: 30%;
+	height: 30%;
 	margin-bottom: 32px;
 `
 
@@ -32,13 +33,12 @@ export const Title = styled(Headline)`
 export const StyledButton = styled(TouchableRipple)`
 	background-color: ${({ theme }) => theme.colors.secondary};
 	elevation: 0;
-	margin-top: 24px;
-	padding-vertical: 16px;
+	padding-vertical: 8px;
 	padding-horizontal: 40px;
 	border-radius: 24px;
 `
 
-export const ButtonText = styled(Headline)`
+export const ButtonText = styled(Subheading)`
 	font-weight: 700;
 	color: ${({ theme }) => theme.title.light};
 	text-transform: uppercase;
@@ -50,4 +50,9 @@ export const Subtitle = styled(Subheading)`
 	font-weight: 500;
 	text-align: center;
 	margin-top: 8px;
+`
+
+export const InputContainer = styled.View`
+	padding: 24px;
+	width: 100%;
 `
--- src/screens/Initial/Initial.tsx
+++ src/screens/Initial/Initial.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
 
 import { useNavigation } from '@react-navigation/core'
-import { getItemAsync } from 'expo-secure-store'
+import { getItemAsync, setItemAsync } from 'expo-secure-store'
+import { Button } from 'react-native'
+import { Snackbar, TextInput } from 'react-native-paper'
 
-import appImage from '../../images/recicle.png'
-import { HOME, MAP } from '../../navigation/routes'
+import appImage from '../../images/icon.png'
+import { HOME, MAP, SIGNUP } from '../../navigation/routes'
+import { api } from '../../services/api'
 import {
 	ButtonText,
 	Container,
+	InputContainer,
 	MainImage,
 	StyledButton,
 	Subtitle,
@@ -17,7 +21,9 @@ import {
 
 export const InitialScreen = () => {
 	const navigation = useNavigation()
+	const [visible, setVisible] = useState(false)
 	const [isNew, setIsNew] = useState<boolean>()
+	const [id, setId] = useState('')
 
 	useEffect(() => {
 		getItemAsync('is_new')
@@ -28,9 +34,17 @@ export const InitialScreen = () => {
 	}, [])
 
 	const handleNavigate = () => {
-		const screenRoute = isNew ? HOME : MAP
+		api
+			.post('/client/login', { accessId: id })
+			.then(async () => {
+				const screenRoute = isNew ? HOME : MAP
 
-		navigation.navigate(screenRoute)
+				await setItemAsync('is_new', 'false')
+				await setItemAsync('token', `Basic ${id}`)
+
+				navigation.navigate(screenRoute)
+			})
+			.catch(() => setVisible(true))
 	}
 
 	return (
@@ -38,11 +52,37 @@ export const InitialScreen = () => {
 			<MainImage source={appImage} />
 			<TextContainer>
 				<Title>UpMeal</Title>
-				<Subtitle>Diminua o desperdício</Subtitle>
+				<Subtitle>Diminuindo o desperdício</Subtitle>
 			</TextContainer>
+			<InputContainer>
+				<TextInput
+					value={id}
+					label="Insira seu ID:"
+					onChangeText={text => setId(text)}
+					placeholder="Ex: b74b0818..."
+				/>
+			</InputContainer>
 			<StyledButton rippleColor="rgba(0, 0, 0, .32)" onPress={() => handleNavigate()}>
 				<ButtonText>Começar</ButtonText>
 			</StyledButton>
+			<Button
+				title="Não tenho cadastro."
+				color="white"
+				onPress={() => navigation.navigate(SIGNUP)}
+			/>
+			<Snackbar
+				visible={visible}
+				onDismiss={() => setVisible(false)}
+				duration={4000}
+				action={{
+					label: 'Fechar',
+					onPress: () => {
+						setVisible(false)
+					}
+				}}
+			>
+				ID inválido ou servidor não está conectado.
+			</Snackbar>
 		</Container>
 	)
 }
